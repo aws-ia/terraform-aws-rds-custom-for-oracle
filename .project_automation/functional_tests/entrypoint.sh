e9dc7e4c8e8c9f5555e5b0b3d7ecddc1a6653f0b
wellsiau-aws <wellsiau@amazon.com>
2023-05-26 15:46:45 -0700

    refactor checkov test

--- .project_automation/functional_tests/entrypoint.sh
+++ .project_automation/functional_tests/entrypoint.sh
@@ -1,5 +1,23 @@
 #!/bin/bash -e
 
+functional_test_checkov() {
+  cd $1
+  terraform init
+  terraform plan -out tf.plan
+  terraform show -json tf.plan  > tf.json 
+  CHECKOV=$(checkov --config-file ${PROJECT_PATH}/.checkov.yml  || true)
+  if [-z "${CHECKOV}" ]
+  then
+    echo "Checkov Analysis Passed"
+    git clean -fxd
+  else
+    echo "Checkov Analysis Failed"
+    echo "$CHECKOV"
+    git clean -fxd
+    exit 1
+  fi
+}
+
 ## NOTE: paths may differ when running in a managed task. To ensure behavior is consistent between
 # managed and local tasks always use these variables for the project and project type path
 PROJECT_PATH=${BASE_PATH}/project
@@ -9,12 +27,15 @@ echo "Starting Functional Tests"
 
 cd ${PROJECT_PATH}
 
+#********** Environment Setup *************
+export AWS_DEFAULT_REGION=us-west-2
+
 #********** Checkov Analysis *************
 echo "Running Checkov Analysis"
-terraform init
-terraform plan -out tf.plan
-terraform show -json tf.plan  > tf.json 
-checkov 
+for dir in ${PROJECT_PATH}/examples/*; do
+  echo "Running Checkov Analysis for: $dir"
+  functional_test_checkov $dir
+done
 
 #********** Terratest execution **********
 echo "Running Terratest"
