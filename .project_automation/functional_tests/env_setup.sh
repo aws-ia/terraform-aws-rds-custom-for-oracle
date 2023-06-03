46d06ebc81960f5c7924892f07bcba49af770f88
wellsiau-aws <wellsiau@amazon.com>
2023-06-02 19:04:51 -0700

    ssm param for kms key

--- .project_automation/functional_tests/env_setup.sh
+++ .project_automation/functional_tests/env_setup.sh
@@ -1,2 +1,20 @@
 #!/bin/bash -e
 export AWS_DEFAULT_REGION=us-west-2
+
+#********** Get tfvars from SSM *************
+echo "Generate tfvars from SSM parameter for rds-instance"
+aws ssm get-parameter \
+  --name "/abp/terraform-aws-rds-custom-for-oracle/cev/kms/key-id" \
+  --with-decryption \
+  --query "Parameter.Value" \
+  --output "text" \
+  --region $AWS_DEFAULT_REGION > ${PROJECT_PATH}/examples/rds-instance/terraform.tfvars
+
+
+echo "Generate tfvars from SSM parameter for rds-instance-and-replica"
+aws ssm get-parameter \
+  --name "/abp/terraform-aws-rds-custom-for-oracle/cev/kms/key-id" \
+  --with-decryption \
+  --query "Parameter.Value" \
+  --output "text" \
+  --region $AWS_DEFAULT_REGION > ${PROJECT_PATH}/examples/rds-instance-and-replica/terraform.tfvars
